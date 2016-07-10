09c8aaa534764713ae9cab45ccd7a676f2996ac3
fscbest <romangeyber@gmail.com>
2016-07-10 22:15:22 +0300

    Add Task object and TaskComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TaskComponent } from './task/task.component'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [TaskComponent]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/task/task.ts
@@ -0,0 +1,12 @@
+export class Task {
+  userId: number;
+  customerId: number;
+  providerId: number;
+  date: Date;
+  title: string;
+  notes: string;
+  price: number;
+  id: number;
+  color: string;
+}
+
