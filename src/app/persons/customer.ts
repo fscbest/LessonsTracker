8f66fca96cf426589df2aa5e8b1b6457e6681995
fscbest <romangeyber@gmail.com>
2016-07-13 20:18:14 +0300

    .

--- src/app/persons/customer.ts
+++ src/app/persons/customer.ts
@@ -1,32 +1,13 @@
-interface ICustomer {
-  userId: number;
-  dateOfBirth: Date;
-  firstName: string;
-  lastName: string;
-  notes: string;
-  id: number;
-  color: string;
-  nickname: string;
-}
-export class Customer implements ICustomer{
-  public userId: number;
-  public dateOfBirth: Date;
-  public firstName: string;
-  public lastName: string;
-  public notes: string;
-  public id: number;
-  public color: string;
-  public nickname: string;
-
-  constructor ();
-  constructor (obj?: ICustomer){
-    this.dateOfBirth = obj && obj.dateOfBirth || new Date('02.02.2002');
-    this.firstName = obj && obj.firstName || "";
-    this.lastName = obj && obj.lastName || "";
-    this.notes = obj && obj.notes || "";
-    this.color = obj && obj.color || "";
-    this.nickname = obj && obj.nickname || "";
-
-
-  }
+export class Customer {
+  constructor (
+    public firstName?: string,
+    public userId?: number,
+    public dateOfBirth?: Date,
+    public lastName?: string,
+    public notes?: string,
+    public id?: number,
+    public color?: string,
+    public nickname?: string
+  )
+  {}
 }
